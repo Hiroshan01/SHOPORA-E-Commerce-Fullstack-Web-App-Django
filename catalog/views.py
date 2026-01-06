2b4abb38e5a3a9ad39d7e12965088482e307eae3
Hiroshan Madusanka <pmhiroshan@gmail.com>
2026-01-06 17:53:07 +0530

    create query sets in backend

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.views.generic import ListView
 
 from .models import Product
@@ -7,4 +8,25 @@ class ProductListView(ListView):
     model = Product
     template_name = "catalog/product_list.html"
     context_object_name = "products"
+    ordering = ("-created_at", "id")
     paginate_by = 12
+
+    def get_queryset(self):
+        qs = Product.objects.filter(is_active=True)
+
+        query = self.request.GET.get("q")
+        sort = self.request.GET.get("sort")
+        min_price = self.request.GET.get("min_price")
+        max_price = self.request.GET.get("max_price")
+
+        if query:
+            qs = qs.filter(Q(name__icontains=query) | Q(description__icontains=query))
+        if min_price:
+            qs = qs.filter(price__gte=int(min_price))
+        if max_price:
+            qs = qs.filter(price__lte=int(max_price))
+        if sort:
+            qs = qs.order_by(sort)
+
+
+        return qs
